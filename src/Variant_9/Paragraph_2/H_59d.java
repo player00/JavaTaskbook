43fea1d0a34d071d01a0ecb790609d9b9744ef63
Alex Heifetz <a.logvinov@carprice.ru>
2018-09-25 18:35:17 +0300

    H_59d

---
+++ src/Variant_9/Paragraph_2/H_59d.java
@@ -0,0 +1,20 @@
+package Variant_9.Paragraph_2;
+
+import Helpers.HTools;
+
+public class H_59d {
+
+    public static void main(String args[]) {
+        double x, y;
+
+        x = Math.abs(HTools.readDouble("Введите x"));
+        y = Math.abs(HTools.readDouble("Введите y"));
+
+        if (x <= 0.5 && y <= 1) {
+            System.out.print("Принадлежит");
+        } else {
+            System.out.print("Не принадлежит");
+        }
+    }
+
+}
