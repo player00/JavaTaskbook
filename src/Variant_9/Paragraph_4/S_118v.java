90c06d0e5f9cded47317f2371a4b6a73f2d80e00
player00 <shalygins23@gmail.com>
2018-09-29 16:17:42 +0300

    118v fix

--- src/Variant_9/Paragraph_4/S_118v.java
+++ src/Variant_9/Paragraph_4/S_118v.java
@@ -4,14 +4,14 @@ public class S_118v {
 
     public static void main(String[] args) {
 
-        double res = 1 / 10000;
+        double res = 1.0 / 10000;
 
         for (double i = 9999; i > 0; i -= 2){
-            res -= 1 / i;
+            res -= 1.0 / i;
         }
 
         for (double i = 9998; i > 0; i -= 2){
-            res += 1 / i;
+            res += 1.0 / i;
         }
 
         System.out.println("Результат: " + res);
