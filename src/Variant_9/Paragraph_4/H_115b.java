94023248f4c775ffbcab89a56040ef7debbbe5ea
Heifetz <Seraph_90@mail.ru>
2018-09-29 15:47:22 +0300

    H_115b

---
+++ src/Variant_9/Paragraph_4/H_115b.java
@@ -0,0 +1,20 @@
+package Variant_9.Paragraph_4;
+
+import Helpers.HTools;
+
+public class H_115b {
+
+    public static void main(String args[]) {
+        int n;
+        double sum = 0;
+
+        n = HTools.readNatural("Введите натуральное число n");
+
+        for (int i = 1; i <= n; i++) {
+            sum += 1 / Math.pow(i, 5);
+        }
+
+        System.out.println("Сумма равна: " + sum);
+    }
+
+}
