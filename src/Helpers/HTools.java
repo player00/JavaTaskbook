6ca918354767fe2573e83f4101d788b0d231a809
Heifetz <Seraph_90@mail.ru>
2018-09-29 14:22:29 +0300

    H_74

--- src/Helpers/HTools.java
+++ src/Helpers/HTools.java
@@ -5,13 +5,69 @@ import java.util.Scanner;
 public class HTools {
     private static Scanner in = new Scanner(System.in);
 
+    /**
+     * Получает рациональное число с консоли.
+     *
+     * @param text Сообщение которое нужно вывести перед вводом
+     * @return double
+     */
     public static double readDouble(String text) {
-        System.out.print(text + "\n");
+        System.out.println(text);
         while (!in.hasNextDouble()) {
             System.out.println("Вы ввели не правильно, попробуйте повторить");
             in.next();
         }
 
         return in.nextDouble();
+    }
+
+    /**
+     * Получает рациональное число с консоли.
+     *
+     * @param text Сообщение которое нужно вывести перед вводом
+     * @param min Минимальное допустимое
+     * @param max Максимальное допустимое
+     * @return double
+     */
+    public static double readDouble(String text, double min, double max) {
+        System.out.println(text + " в промежутке от " + min + " до " + max);
+        while (!in.hasNextDouble()) {
+            System.out.println("Вы ввели не правильно, попробуйте повторить");
+            in.next();
+        }
+
+        double num = in.nextDouble();
+
+        while (!(min <= num && num <= max)) {
+            System.out.println("Вы ввели не правильно, попробуйте повторить");
+            num = in.nextDouble();
+        }
+
+        return num;
+    }
+
+    /**
+     * Получает целое число с консоли.
+     *
+     * @param text Сообщение которое нужно вывести перед вводом
+     * @param min Минимальное допустимое
+     * @param max Максимальное допустимое
+     * @return double
+     */
+    public static int readInt(String text, int min, int max) {
+        System.out.println(text + " в промежутке от " + min + " до " + max);
+        while (!in.hasNextInt()) {
+            System.out.println("Вы ввели не правильно, попробуйте повторить");
+            in.next();
+        }
+
+        int num = in.nextInt();
+
+        while (!(min <= num && num <= max)) {
+            System.out.println("Вы ввели не правильно, попробуйте повторить");
+            num = in.nextInt();
+        }
+
+        return num;
     }
 }
---
+++ src/Variant_9/Paragraph_3/H_74.java
@@ -0,0 +1,36 @@
+package Variant_9.Paragraph_3;
+
+import Helpers.HTools;
+
+public class H_74 {
+
+    public static void main(String args[]) {
+        int age;
+        String ageString, ageLast;
+
+        age = HTools.readInt("Введите возраст", 0, 100);
+
+        ageString = Integer.toString(age);
+        ageLast = ageString.substring(ageString.length() - 1);
+
+        if (11 <= age && age <= 15) {
+            ageString += " лет";
+        } else {
+            switch (ageLast) {
+                case "1":
+                    ageString += " год";
+                    break;
+                case "2":
+                case "3":
+                case "4":
+                    ageString += " года";
+                    break;
+                default:
+                    ageString += " лет";
+                    break;
+            }
+        }
+        System.out.println(ageString);
+    }
+
+}
