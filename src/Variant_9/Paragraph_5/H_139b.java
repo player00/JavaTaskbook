87375e29fda87474a6ac9e6fe013cee895c77ec3
Heifetz <Seraph_90@mail.ru>
2018-09-29 16:49:38 +0300

    H_139b

---
+++ src/Variant_9/Paragraph_5/H_139b.java
@@ -0,0 +1,20 @@
+package Variant_9.Paragraph_5;
+
+import Helpers.HTools;
+
+public class H_139b {
+
+    public static void main(String args[]) {
+        int n = HTools.readNatural();
+        double[] b = new double[n];
+
+        for (int i = 1; i <= n; i++) {
+            b[i-1] = i * i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("b[" + (i + 1) + "]=" + b[i]);
+        }
+    }
+
+}
